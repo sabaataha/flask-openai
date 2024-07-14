8f3220906c444e38a94294c1383b6d624dfab140
sabaataha <sabaataha@gmail.com>
2024-07-14 23:58:32 +0300

    added openai api to get answer for question request

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask , request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from openai import OpenAI
+from db.db_init import init_db, db  
+from db.models import Question 
+import os  ,openai , psycopg2
 
 
 app = Flask(__name__)
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI()
 
 
 @app.route("/")
@@ -17,7 +24,19 @@ def ask_question():
     
     if not question_text:
         return jsonify({'error': 'No question provided'}), 400
-    return jsonify({'question_text': question_text}), 200
+    try:
+        response = openai.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": question_text}]
+        )
+        answer_text = response.choices[0].message.content
+        new_question = Question(question=question_text, answer=answer_text)
+        db.session.add(new_question)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+    return jsonify({'answer_text': answer_text}), 200
 
 
 
