c3f8b42e76b36a7c48a923c097dd60c22962d99b
sabaataha <sabaataha@gmail.com>
2024-07-15 11:51:18 +0300

    fixed DAL structure, added dal.py file

---
+++ db/dal.py
@@ -0,0 +1,11 @@
+from db.db_init import db
+from db.models import Question
+
+def add_question(question_text, answer_text):
+    new_question = Question(question=question_text, answer=answer_text)
+    db.session.add(new_question)
+    db.session.commit()
+
+def get_all_questions():
+    questions = Question.query.all()
+    return [{'id': q.id, 'question': q.question, 'answer': q.answer} for q in questions]
